c56517904898051cb4a4f00ecf0d3d0eb04fc6b7
ian@themeshaper.com <ian@themeshaper.com@25245320-ac48-0410-b4b3-eb13123354aa>
2009-02-21 17:52:46 +0000

    Filtered the menu args and added a home link. Serves as a good example.

--- child-themes/thematic-classic/functions.php
+++ child-themes/thematic-classic/functions.php
@@ -2,9 +2,21 @@
 
 // Filter away the default scripts loaded with Thematic
 function childtheme_head_scripts() {
-    // Abscence makes the heart grow fonder.
+    // Abscence makes the heart grow fonder
 }
 add_filter('thematic_head_scripts','childtheme_head_scripts');
+
+// Adds a home link to your menu
+function childtheme_menu_args($args) {
+    $args = array(
+        'show_home' => 'Home',
+        'sort_column' => 'menu_order',
+        'menu_class' => 'menu',
+        'echo' => true
+    );
+	return $args;
+}
+add_filter('wp_page_menu_args','childtheme_menu_args');
 
 
 ?>
